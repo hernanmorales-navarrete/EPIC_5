bcf4894037f1c737a872553048d377dea0abbcf6
Wladimir Banda Barragan <wbandabarragan@Wladimirs-MacBook-Pro.local>
2025-03-25 13:05:58 -0500

    update

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,49 @@
 // Custom Script
 // Developed by: Samson.Onna
-// CopyRights : http://webthemez.com
 var customScripts = {
     profile: function () {
-     	 var portfolio = $('#portfolio');
-		var items = $('.items', portfolio); 
-		var filters = $('.filters li a', portfolio); 
-	
-		items.imagesLoaded(function() {
-			items.isotope({
-				itemSelector: '.item',
-				layoutMode: 'fitRows',
-				transitionDuration: '0.7s'
-			});
-		});
-		
-		filters.click(function(){
-			var el = $(this);
-			filters.removeClass('active');
-			el.addClass('active');
-			var selector = el.attr('data-filter');
-			items.isotope({ filter: selector });
-			return false;
-		});            
+        // portfolio
+        if ($('.isotopeWrapper').length) {
+            var $container = $('.isotopeWrapper');
+            var $resize = $('.isotopeWrapper').attr('id');
+            // initialize isotope
+            $container.isotope({
+                itemSelector: '.isotopeItem',
+                resizable: false, // disable normal resizing
+                masonry: {
+                    columnWidth: $container.width() / $resize
+                }
+            });
+            $("a[href='#top']").click(function () {
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+                return false;
+            });
+            $('.navbar-inverse').on('click', 'li a', function () {
+                $('.navbar-inverse .in').addClass('collapse').removeClass('in').css('height', '1px');
+            });
+            $('#filter a').click(function () {
+                $('#filter a').removeClass('current');
+                $(this).addClass('current');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 1000,
+                        easing: 'easeOutQuart',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+            $(window).smartresize(function () {
+                $container.isotope({
+                    // update columnWidth to a percentage of container width
+                    masonry: {
+                        columnWidth: $container.width() / $resize
+                    }
+                });
+            });
+        }
     },
     fancybox: function () {
         // fancybox
@@ -30,7 +51,10 @@ var customScripts = {
     },
     onePageNav: function () {
 
-        $('#mainNav').onePageNav({
+        		if($('#main-nav ul li:first-child').hasClass('active')){
+					$('#main-nav').css('background','none');
+		}
+        $('#mainNav').onePageNav({        
             currentClass: 'active',
             changeHash: false,
             scrollSpeed: 950,
@@ -39,11 +63,12 @@ var customScripts = {
             easing: 'swing',
             begin: function () {
                 //I get fired when the animation is starting
+				
             },
             end: function () {
-                   //I get fired when the animation is ending
+                //I get fired when the animation is ending
 				if(!$('#main-nav ul li:first-child').hasClass('active')){
-					$('.header').addClass('addBg');
+					$('.header').addClass('addBg');					
 				}else{
 						$('.header').removeClass('addBg');
 				}
@@ -56,18 +81,15 @@ var customScripts = {
 				}else{
 						$('.header').removeClass('addBg');
 				}
-			}
+            }
         });
-		
-		$("a[href='#top']").click(function () {
-                $("html, body").animate({ scrollTop: 0 }, "slow");
-                return false;
-            });
-			$("a[href='#basics']").click(function () {
-                $("html, body").animate({ scrollTop: $('#services').offset().top - 75 }, "slow"); 
-                return false;
-            });
-    }, 
+    },
+    slider: function () {
+        $('#da-slider').cslider({
+            autoplay: true,
+            bgincrement: 0
+        });
+    },
     owlSlider: function () {
         var owl = $("#owl-demo");
         owl.owlCarousel();
@@ -87,87 +109,43 @@ var customScripts = {
             $('#da-slider').height(bHeight);
         });
     },
-	waySlide: function(){
-		  	/* Waypoints Animations
-		   ------------------------------------------------------ */		   			  
-		 
-			 						 
-		},
-		fitText: function(){			  
-				setTimeout(function() {			
-				$('h1.responsive-headline').fitText(1.2, { minFontSize: '16px', maxFontSize: '30px' });			
-				}, 100);
-		},
     init: function () {
         customScripts.onePageNav();
         customScripts.profile();
-        customScripts.fancybox(); 
+        customScripts.fancybox();
+        customScripts.slider();
         customScripts.owlSlider();
-		customScripts.waySlide();
-		customScripts.fitText();
         customScripts.bannerHeight();
     }
 }
 $('document').ready(function () {
     customScripts.init();
-	/*----------------------------------------------------*/
-/*	gmaps
-------------------------------------------------------*/
+	$('#diagram-id-1').diagram({ 
+			size: "190",
+			borderWidth: "10",
+			bgFill: "#95a5a6",
+			frFill: "#3498db",
+			textSize: 54,
+			textColor: '#1a1a1a'
+		}); 
+		$('#diagram-id-2').diagram({ 
+			size: "190",
+			borderWidth: "10",
+			bgFill: "#95a5a6",
+			frFill: "#3498db",
+			textSize: 54,
+			textColor: '#333'
+		});
 
-   var map;
-
-   // main directions
-   map = new GMaps({
-      el: '#map', lat: 14.553533, lng: 121.052124, zoom: 14, zoomControl : true,
-      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
-   });
-
-   // add address markers
-   map.addMarker({ lat: 14.552891, lng: 121.051815, title: 'WebThemez',
-   infoWindow: { content: '<p>WebThemez</p>' } });
-
-    $('#main-slider').flexslider({
-        namespace           : "flex-",           //{NEW} String: Prefix string attached to the class of every element generated by the plugin
-        selector            : ".slides > li",    //{NEW} Selector: Must match a simple pattern. '{container} > {slide}' -- Ignore pattern at your own peril
-        animation           : "fade",            //String: Select your animation type, "fade" or "slide"
-        easing              : "swing",           //{NEW} String: Determines the easing method used in jQuery transitions. jQuery easing plugin is supported!
-        direction           : "horizontal",      //String: Select the sliding direction, "horizontal" or "vertical"
-        reverse             : false,             //{NEW} Boolean: Reverse the animation direction
-        animationLoop       : true,              //Boolean: Should the animation loop? If false, directionNav will received "disable" classes at either end
-        smoothHeight        : false,             //{NEW} Boolean: Allow height of the slider to animate smoothly in horizontal mode
-        startAt             : 0,                 //Integer: The slide that the slider should start on. Array notation (0 = first slide)
-        slideshow           : true,              //Boolean: Animate slider automatically
-        slideshowSpeed      : 7000,              //Integer: Set the speed of the slideshow cycling, in milliseconds
-        animationSpeed      : 600,               //Integer: Set the speed of animations, in milliseconds
-        initDelay           : 0,                 //{NEW} Integer: Set an initialization delay, in milliseconds
-        randomize           : false,             //Boolean: Randomize slide order
-         
-        // Usability features
-        pauseOnAction       : true,              //Boolean: Pause the slideshow when interacting with control elements, highly recommended.
-        pauseOnHover        : false,             //Boolean: Pause the slideshow when hovering over slider, then resume when no longer hovering
-        useCSS              : true,              //{NEW} Boolean: Slider will use CSS3 transitions if available
-        touch               : true,              //{NEW} Boolean: Allow touch swipe navigation of the slider on touch-enabled devices
-        video               : false,             //{NEW} Boolean: If using video in the slider, will prevent CSS3 3D Transforms to avoid graphical glitches
-         
-        // Primary Controls
-        controlNav          : true,              //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
-        directionNav        : true,              //Boolean: Create navigation for previous/next navigation? (true/false)
-        prevText            : "Previous",        //String: Set the text for the "previous" directionNav item
-        nextText            : "Next",            //String: Set the text for the "next" directionNav item
-         
-        // Secondary Navigation
-        keyboard            : true,              //Boolean: Allow slider navigating via keyboard left/right keys
-        multipleKeyboard    : false,             //{NEW} Boolean: Allow keyboard navigation to affect multiple sliders. Default behavior cuts out keyboard navigation with more than one slider present.
-        mousewheel          : false,             //{UPDATED} Boolean: Requires jquery.mousewheel.js (https://github.com/brandonaaron/jquery-mousewheel) - Allows slider navigating via mousewheel
-        pausePlay           : false,             //Boolean: Create pause/play dynamic element
-        pauseText           : 'Pause',           //String: Set the text for the "pause" pausePlay item
-        playText            : 'Play',            //String: Set the text for the "play" pausePlay item
-         
-        // Special properties
-        controlsContainer   : "",                //{UPDATED} Selector: USE CLASS SELECTOR. Declare which container the navigation elements should be appended too. Default container is the FlexSlider element. Example use would be ".flexslider-container". Property is ignored if given element is not found.
-        manualControls      : "",                //Selector: Declare custom control navigation. Examples would be ".flex-control-nav li" or "#tabs-nav li img", etc. The number of elements in your controlNav should match the number of slides/tabs.
-        sync                : "",                //{NEW} Selector: Mirror the actions performed on this slider with another slider. Use with care.
-        asNavFor            : "",                //{NEW} Selector: Internal property exposed for turning the slider into a thumbnail navigation for another slider
-    });
-
+		$('#diagram-id-3').diagram({ 
+			size: "190",
+			borderWidth: "10",
+			bgFill: "#95a5a6",
+			frFill: "#3498db",
+			textSize: 54,
+			textColor: '#1a1a1a'
+		});
+		$(window).load(function() { 
+			  $('#filter .current').trigger('click');
+		});
 });
